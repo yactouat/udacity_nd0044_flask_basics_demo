6a1281a1b2d0dc4a3b27f7e824f1eab6517a08d5
yactouat <yactouat@hotmail.com>
2022-09-18 11:15:45 +0200

    adding a custom logger to the app'

--- app/init.py
+++ app/init.py
@@ -1,9 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from logging.config import dictConfig
 
 from routes import init_routes
 
 
 def create_app(test_config=None):
+
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'app_logger': {
+            'class': 'logging.FileHandler',
+            'filename': 'logs.log',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['app_logger']
+        }
+    })
 
     # creates an application that is named after the name of the file
     app = Flask(__name__)
@@ -11,12 +28,17 @@ def create_app(test_config=None):
     app.config["SECRET_KEY"] = "some_dev_key"
     # alternative configuration based on if is test env or not
     if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://usr:pwd@pgsql:5432/todos"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"
+        + "usr:pwd@pgsql:5432/todos"
     elif test_config == "test":
-        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://usr:pwd@pgsql-test:5433/todos"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://"
+        + "usr:pwd@pgsql-test:5433/todos"
 
     @app.errorhandler(404)
     def not_found(error):
+        app.logger.info(
+            f"user tried to access route {request.full_path}"
+        )
         return jsonify({
             "msg": "resource not found, aborting...",
             "success": False,
